fff988ff5e4833ea3ada2c64438f2eeb932d0cca
zimooh <zimooh@gmail.com>
2023-07-19 00:11:23 +0300

    4.FaqSection

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -1,12 +1,14 @@
 //import page components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSections";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />;
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
---
+++ src/components/FaqSection.tsx
@@ -0,0 +1,52 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        {" "}
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="questions">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            consequatur?
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>Daily Schedule?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            consequatur?
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            consequatur?
+          </p>
+        </div>
+      </div>
+      <div className="questions">
+        <h4>what products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            consequatur?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
