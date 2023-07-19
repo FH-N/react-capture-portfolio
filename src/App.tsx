338ec0185af2cb2e1ac74071d5ade6acde20a51c
zimooh <zimooh@gmail.com>
2023-07-19 17:07:08 +0300

    8.Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 //import pages
 import AboutUs from "./Pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />;
     </div>
   );
