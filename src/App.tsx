ca2143cd1ac34eaa0ea713d33d00584840491bb8
zimooh <zimooh@gmail.com>
2023-07-20 01:11:08 +0300

    9.React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 //import pages
 import AboutUs from "./Pages/AboutUs";
-import GlobalStyle from "./components/GlobalStyle";
+import OurWork from "./Pages/OurWork";
+import ContactUs from "./Pages/ContactUs";
 import Nav from "./components/Nav";
+//Import styles
+import GlobalStyle from "./components/GlobalStyle";
+//Import Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />;
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />;
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
