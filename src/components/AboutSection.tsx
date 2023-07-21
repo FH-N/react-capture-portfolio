555d8f4597e6fc4f3029f2610f0c2c63ae059921
FH-N <faridanasrat@gmail.com>
2023-07-22 00:08:41 +0300

    14. About Animation

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,6 +2,7 @@ import home1 from "../img/home1.jpg";
 import { Layout, Description, Image, Hide } from "../styles";
 //framer motion 
 import {motion} from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 
 const AboutSection = () => {
@@ -12,25 +13,25 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals that make things come to life.
-        </p>
-        <button>Contact us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="girl holding camera" />
+        <motion.img variants={photoAnim} src={home1} alt="girl holding camera" />
       </Image>
     </Layout>
   );
