eba941e9b7f6a747862080738194a0722c2fb86f
FH-N <faridanasrat@gmail.com>
2023-07-21 13:01:08 +0300

    11.5 Movie Details

--- src/Pages/MovieDetail.tsx
+++ src/Pages/MovieDetail.tsx
@@ -1,13 +1,27 @@
 //import styled from "styled-components"
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import {useHistory} from "react-router-dom"
 import { MovieState } from "../movieState";
 
 const MovieDetail = () => {
+
+    /*interface MovieStateProps {
+        title: string;
+      mainImg: string;
+      secondaryImg: string;
+      url: string;
+      awards: {
+        title: string;
+        description: string;
+      }[];
+      }[]*/
+
+    
     const history = useHistory();
     const url = history.location.pathname;
-    const [movies, setMovies] useState<{MovieState}>;
-    const [movie, setMovie] useState(null);
+    //const [movies, setMovies] = useState<MovieStateProps>({MovieState});
+    const [movies, setMovies] = useState(MovieState);
+    const [movie, setMovie] = useState(null);
 //useEffect
 useEffect(() => {
     const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
